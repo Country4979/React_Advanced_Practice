a8eee8f90821f8d9b91cc68f0095c019620fd315
Javier Girón <115418134+Country4979@users.noreply.github.com>
2023-06-06 21:25:16 +0200

    Redux DevTools Extenision Configured

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,9 +2,12 @@ import { createStore } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
 import reducer from './reducers';
 
+import * as actionCreators from './actions'
 
-
+const composeEnhancers = composeWithDevTools({
+    actionCreators,
+})
 export default function configureStore() {
-    const store = createStore(reducer, composeWithDevTools());
+    const store = createStore(reducer, composeEnhancers());
     return store
 }
