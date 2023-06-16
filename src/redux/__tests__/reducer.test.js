ff65a418bc2ba545202475a684da8767e25dd2c4
Javier Girón <115418134+Country4979@users.noreply.github.com>
2023-06-16 20:39:04 +0200

    reducer Test OK

--- src/redux/__tests__/reducer.test.js
+++ src/redux/__tests__/reducer.test.js
@@ -1,1 +1,23 @@
-describe('Testing React Redux Reducer', () => {});
+import { authLoginFailure, authLoginSuccess } from '../actions';
+import { auth } from '../reducers';
+
+describe('Testing React Redux Reducer', () => {
+    describe('"auth" reducer test', () => {
+        var state;
+        beforeEach(() => {
+            state = false;
+        });
+        //Define the test cases with the different actions and expected values.
+        const testCases = [
+            [true, 'LOGIN_SUCCESS', authLoginSuccess()],
+            [false, 'LOGIN_FAILURE', authLoginFailure()],
+        ];
+        //Iterating about test cases.
+        test.each(testCases)(
+            'returns %s when the action is %s',
+            (expected, actionName, action) => {
+                expect(auth(state, action)).toBe(expected);
+            }
+        );
+    });
+});
